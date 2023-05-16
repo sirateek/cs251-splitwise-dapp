15669bfa06f9b99ab8e939b230b42bff41e2ebe2
Siratee K <sirateek@gmail.com>
2023-05-16 13:17:47 +0700

    Initial COmmit

---
+++ contracts/mycontract.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+
+// DO NOT MODIFY BELOW THIS
+pragma solidity ^0.8.17;
+
+import "hardhat/console.sol";
+
+contract Splitwise {
+    // DO NOT MODIFY ABOVE THIS
+}
